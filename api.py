9e4f97f2735b1d5dd5631aa92f24d500b071ecb2
cedlautrichien <98392989+cedlautrichien@users.noreply.github.com>
2025-02-11 17:55:17 +0100

    removed json stuff, improved dynamic xml

--- api.py
+++ api.py
@@ -1,4 +1,11 @@
-from flask import Flask, request, Response, render_template, jsonify, send_from_directory
+from flask import (
+    Flask,
+    request,
+    Response,
+    render_template,
+    jsonify,
+    send_from_directory,
+)
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,10 +22,10 @@ app = Flask(__name__)
 #       db.create_all()
 
 
-@app.route("/get_static_xml")
+@app.route("/example_xml")
 def get_static_xml():
-    type = request.args.get("type")
-    if type == "014C":
+    message_type = request.args.get("messageType")
+    if message_type == "CC014C":
         xml_content = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><CC014C xmlns:ns2="urn:eds:datamodel:EDS:EDS_EXTENSIONS:1" xmlns:ns3="http://ncts.dgtaxud.ec"><Extensions><SequenceNumeric>1</SequenceNumeric><Key>HolderOfTheTransitProcedureRIN</Key><Value>ATRIN4952418247</Value><DataType>text</DataType></Extensions><messageSender>swp.transit.agent</messageSender><messageRecipient>NTA.AT</messageRecipient><preparationDateAndTime>2024-12-20T15:11:53</preparationDateAndTime><messageIdentification>100788_151153</messageIdentification><messageType>CC014C</messageType><correlationIdentifier>test</correlationIdentifier><TransitOperation><MRN>24AT100000Y5L4I0K0</MRN></TransitOperation><Invalidation><requestDateAndTime>2024-12-20T15:11:53</requestDateAndTime><initiatedByCustoms>0</initiatedByCustoms><justification>darum</justification></Invalidation><CustomsOfficeOfDeparture><referenceNumber>AT100000</referenceNumber></CustomsOfficeOfDeparture><HolderOfTheTransitProcedure><identificationNumber>ATEOS9999999991</identificationNumber><ContactPerson><name>Claus Thorup</name><phoneNumber>2105577890</phoneNumber><eMailAddress>cthor@email.gr</eMailAddress></ContactPerson></HolderOfTheTransitProcedure></CC014C>'
         resp = Response(xml_content, mimetype="application/xml")
         return resp
@@ -26,27 +33,32 @@ def get_static_xml():
         return "No message template found"
 
 
-@app.route("/get_dynamic_xml")
+@app.route("/dynamic_xml")
 def get_dynamic_xml():
-    type = request.args.get("type")
-    if type == "014C":
+    message_type = request.args.get("messageType")
+    holder_rin = request.args.get("holderRIN")
+    mrn = request.args.get("MRN")
+    oodep = request.args.get("OoDep")
+    holder_eori = request.args.get("holderEORI")
+
+    if message_type == "CC014C":
         object = {
             "SequenceNumeric": "1",
             "Key": "HolderOfTheTransitProcedureRIN",
-            "Value": "ATRIN4952418247",
+            "Value": holder_rin,
             "DataType": "text",
-            "messageSender": "swp.transit.agent",
+            "messageSender": "Wirtschaftsbeteiligter",
             "messageRecipient": "NTA.AT",
             "preparationDateAndTime": "2024-12-20T15:11:53",
             "messageIdentification": "100788_151153",
-            "messageType": "CC014C",
+            "messageType": message_type,
             "correlationIdentifier": "test",
-            "MRN": "24AT100000Y5L4I0K0",
+            "MRN": mrn,
             "requestDateAndTime": "2024-12-20T15:11:53",
             "initiatedByCustoms": "0",
-            "justification": "darum",
-            "referenceNumber": "AT100000",
-            "identificationNumber": "ATEOS9999999991",
+            "justification": "test",
+            "referenceNumber": oodep,
+            "identificationNumber": holder_eori,
             "name": "Claus Thorup",
             "phoneNumber": "2105577890",
             "eMailAddress": "cthor@email.gr",
@@ -59,31 +71,33 @@ def get_dynamic_xml():
         return "No message template found"
 
 
-@app.route("/post_json_data", methods=["POST"])
-def post_json_data():
-    type = request.args.get("type")
-    if type == "014C":
-
-        if request.is_json:
-            object = request.get_json()
-        else:
-            return jsonify({"error": "Invalid requested, expected JSON"}), 400
-
-        xml_content = render_template("014C.xml", object=object)
-        return Response(xml_content, mimetype="application/xml")
-
-    else:
-        return "No message template found"
+# @app.route("/custom_json", methods=["POST"])
+# def post_json_data():
+#    type = request.args.get("type")
+#    if type == "014C":
+#
+#        if request.is_json:
+#            object = request.get_json()
+#        else:
+#            return jsonify({"error": "Invalid requested, expected JSON"}), 400
+#
+#        xml_content = render_template("014C.xml", object=object)
+#        return Response(xml_content, mimetype="application/xml")
+#
+#    else:
+#        return "No message template found"
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 # Serve static files (Swagger UI assets)
 @app.route("/static/<path:filename>")
 def static_files(filename):
     return send_from_directory("static/swagger-ui", filename)
+
 
 # Serve OpenAPI YAML file
 @app.route("/openapi.yaml")
