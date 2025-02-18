dffab3705fa274fde42442634b4249bea664ed70
cedlautrichien <98392989+cedlautrichien@users.noreply.github.com>
2025-02-18 16:48:51 +0100

    added 007C TIR

--- api.py
+++ api.py
@@ -22,90 +22,126 @@ app = Flask(__name__)
 #       db.create_all()
 
 
-@app.route("/generic_xml")
-def get_static_xml():
-    message_type = request.args.get("messageType")
-    if message_type == "CC014C":
-        xml_content = '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?><CC014C xmlns:ns3="http://ncts.dgtaxud.ec"><messageSender>swp.transit.agent</messageSender><messageRecipient>NTA.AT</messageRecipient><preparationDateAndTime>2024-12-20T15:11:53</preparationDateAndTime><messageIdentification>100788151153</messageIdentification><messageType>CC014C</messageType><correlationIdentifier>test</correlationIdentifier><TransitOperation><MRN>24AT100000Y5L4I0K0</MRN></TransitOperation><Invalidation><requestDateAndTime>2024-12-20T15:11:53</requestDateAndTime><initiatedByCustoms>0</initiatedByCustoms><justification>darum</justification></Invalidation><CustomsOfficeOfDeparture><referenceNumber>AT100000</referenceNumber></CustomsOfficeOfDeparture><HolderOfTheTransitProcedure><identificationNumber>ATEOS9999999991</identificationNumber><ContactPerson><name>Claus Thorup</name><phoneNumber>2105577890</phoneNumber><eMailAddress>cthor@email.gr</eMailAddress></ContactPerson></HolderOfTheTransitProcedure></CC014C>'
-        resp = Response(xml_content, mimetype="application/xml")
-        return resp
-    else:
-        return "No message template found"
-
-
-@app.route("/austrian_xml")
-def get_dynamic_xml():
+@app.route("/CC014C")
+def get_CC014C():
     message_type = request.args.get("messageType")
     holder_rin = request.args.get("holderRIN")
     mrn = request.args.get("MRN")
     oodep = request.args.get("OoDep")
     holder_eori = request.args.get("holderEORI")
-    simplified=request.args.get("simplified")
-    authorisation=request.args.get("authorisation")
-    oodes=request.args.get("OoDes")
-    trader_eori=request.args.get("traderEORI")
-    location_type=request.args.get("typeOfLocation")
-    qualifier_id=request.args.get("qualifierID")
-    trader_rin=request.args.get("traderRIN")
-    lid=request.args.get("locationID")
 
-    if message_type == "CC014C":
-        object = {
-            "SequenceNumeric": "1",
-            "Key": "HolderOfTheTransitProcedureRIN",
-            "Value": holder_rin,
-            "DataType": "text",
-            "messageSender": "Wirtschaftsbeteiligter",
-            "messageRecipient": "NTA.AT",
-            "preparationDateAndTime": "2024-12-20T15:11:53",
-            "messageIdentification": "100788_151153",
-            "messageType": message_type,
-            "correlationIdentifier": "test",
-            "MRN": mrn,
-            "requestDateAndTime": "2024-12-20T15:11:53",
-            "initiatedByCustoms": "0",
-            "justification": "test",
-            "referenceNumber": oodep,
-            "identificationNumber": holder_eori,
-            "name": "test",
-            "phoneNumber": "123456780",
-            "eMailAddress": "test@test.com",
-        }
+    object = {
+        "SequenceNumeric": "1",
+        "Key": "HolderOfTheTransitProcedureRIN",
+        "Value": holder_rin,
+        "DataType": "text",
+        "messageSender": "Wirtschaftsbeteiligter",
+        "messageRecipient": "NTA.AT",
+        "preparationDateAndTime": "2024-12-20T15:11:53",
+        "messageIdentification": "100788_151153",
+        "messageType": message_type,
+        "correlationIdentifier": "test",
+        "MRN": mrn,
+        "requestDateAndTime": "2024-12-20T15:11:53",
+        "initiatedByCustoms": "0",
+        "justification": "test",
+        "referenceNumber": oodep,
+        "identificationNumber": holder_eori,
+        "name": "test",
+        "phoneNumber": "123456780",
+        "eMailAddress": "test@test.com",
+    }
 
-        xml_content = render_template("014C.xml", object=object)
-        return Response(xml_content, mimetype="application/xml")
+    xml_content = render_template("014C.xml", object=object)
+    return Response(xml_content, mimetype="application/xml")
 
-    if message_type == "CC007C":
-        object = {
-            "SequenceNumeric": "1",
-            "Key": "HolderOfTheTransitProcedureRIN",
-            "Value": trader_rin,
-            "DataType": "text",
-            "messageSender": "Wirtschaftsbeteiligter",
-            "messageRecipient": "NTA.AT",
-            "preparationDateAndTime": "2024-12-20T15:11:53",
-            "messageIdentification": "100788_151153",
-            "messageType": message_type,
-            "correlationIdentifier": "test",
-            "MRN": mrn,
-            "arrivalNotificationDateAndTime":"2024-12-20T15:11:53",
-            "simplifiedProcedure": simplified,
-            "incidentFlag":"0",
-            "sequenceNumber": "1",
-            "type":"C520",
-            "referenceNumber": authorisation,
-            "referenceNumber2": oodes,
-            "identificationNumber": trader_eori,
-            "typeOfLocation":location_type,
-            "qualifierOfIdentification":qualifier_id,
-            "authorisationNumber":lid
-        }
 
-        xml_content = render_template("007C.xml", object=object)
-        return Response(xml_content, mimetype="application/xml")
+@app.route("/CC007C")
+def get_CC007C():
+    simplified = request.args.get("simplified")
+    authorisation = request.args.get("authorisationID")
+    oodes = request.args.get("OoDes")
+    trader_eori = request.args.get("traderEORI")
+    location_type = request.args.get("typeOfLocation")
+    qualifier_id = request.args.get("qualifierID")
+    trader_rin = request.args.get("traderRIN")
+    lid = request.args.get("locationID")
+    message_type = request.args.get("messageType")
+    mrn = request.args.get("MRN")
 
-    else:
-        return "No message template found"
+    object = {
+        "SequenceNumeric": "1",
+        "Key": "HolderOfTheTransitProcedureRIN",
+        "Value": trader_rin,
+        "DataType": "text",
+        "messageSender": "Wirtschaftsbeteiligter",
+        "messageRecipient": "NTA.AT",
+        "preparationDateAndTime": "2024-12-20T15:11:53",
+        "messageIdentification": "100788_151153",
+        "messageType": message_type,
+        "correlationIdentifier": "test",
+        "MRN": mrn,
+        "arrivalNotificationDateAndTime": "2024-12-20T15:11:53",
+        "simplifiedProcedure": simplified,
+        "incidentFlag": "0",
+        "sequenceNumber": "1",
+        "type": "C520",
+        "referenceNumber": authorisation,
+        "referenceNumber2": oodes,
+        "identificationNumber": trader_eori,
+        "typeOfLocation": location_type,
+        "qualifierOfIdentification": qualifier_id,
+        "authorisationNumber": lid,
+    }
+
+    xml_content = render_template("007C.xml", object=object)
+    return Response(xml_content, mimetype="application/xml")
+
+
+@app.route("/CC007C_TIR")
+def get_CC007C_TIR():
+    simplified = request.args.get("simplified")
+    authorisation1 = request.args.get("authorisationID1")
+    authorisation2 = request.args.get("authorisationID2")
+    oodes = request.args.get("OoDes")
+    trader_eori = request.args.get("traderEORI")
+    location_type = request.args.get("typeOfLocation")
+    qualifier_id = request.args.get("qualifierID")
+    trader_rin = request.args.get("traderRIN")
+    lid = request.args.get("locationID")
+    message_type = request.args.get("messageType")
+    mrn = request.args.get("MRN")
+
+    object = {
+        "SequenceNumeric": "1",
+        "Key": "HolderOfTheTransitProcedureRIN",
+        "Value": trader_rin,
+        "DataType": "text",
+        "messageSender": "Wirtschaftsbeteiligter",
+        "messageRecipient": "NTA.AT",
+        "preparationDateAndTime": "2024-12-20T15:11:53",
+        "messageIdentification": "100788_151153",
+        "messageType": message_type,
+        "correlationIdentifier": "test",
+        "MRN": mrn,
+        "arrivalNotificationDateAndTime": "2024-12-20T15:11:53",
+        "simplifiedProcedure": simplified,
+        "incidentFlag": "0",
+        "sequenceNumber1": "1",
+        "type1": "C520",
+        "referenceNumber1": authorisation1,
+        "sequenceNumber2": "2",
+        "type2": "C522",
+        "referenceNumber2": authorisation2,
+        "referenceNumber3": oodes,
+        "identificationNumber": trader_eori,
+        "typeOfLocation": location_type,
+        "qualifierOfIdentification": qualifier_id,
+        "authorisationNumber": lid,
+    }
+
+    xml_content = render_template("007C_TIR.xml", object=object)
+    return Response(xml_content, mimetype="application/xml")
 
 
 @app.route("/")
